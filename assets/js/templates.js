b95aa8900805dd68e0c63b3508471073bd1ef644
Tibor <tibor@tk305.com>
2018-06-19 17:39:04 +0200

    init

---
+++ assets/js/templates.js
@@ -0,0 +1,148 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['about'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<!-- about -->\n<div class=\"about\">\n	<div class=\"intro intro--about\">\n		<div class=\"intro__container\">\n			<h1 class=\"about__heading animate animate--fade-in\">\n				"
+    + ((stack1 = ((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper))) != null ? stack1 : "")
+    + "\n			</h1>\n			<p class=\"intro__description animate animate--fade-in animate--delay-short\">\n				"
+    + alias4(((helper = (helper = helpers.p1 || (depth0 != null ? depth0.p1 : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"p1","hash":{},"data":data}) : helper)))
+    + "\n			</p>\n			<p class=\"intro__description intro__description--small animate animate--fade-in animate--delay-medium\">\n				"
+    + alias4(((helper = (helper = helpers.p2 || (depth0 != null ? depth0.p2 : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"p2","hash":{},"data":data}) : helper)))
+    + "\n			</p>\n			<p class=\"intro__description intro__description--small animate animate--fade-in animate--delay-long\">\n				"
+    + alias4(((helper = (helper = helpers.p3 || (depth0 != null ? depth0.p3 : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"p3","hash":{},"data":data}) : helper)))
+    + "\n			</p>\n			<img data-src=\"media/"
+    + alias4(((helper = (helper = helpers.img || (depth0 != null ? depth0.img : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"img","hash":{},"data":data}) : helper)))
+    + "\" class=\"img-responsive about__img js-about-img\" />\n			<p class=\"intro__description intro__description--small animate animate--fade-in animate--delay-very-long\">\n				"
+    + alias4(((helper = (helper = helpers.p4 || (depth0 != null ? depth0.p4 : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"p4","hash":{},"data":data}) : helper)))
+    + "\n				"
+    + alias4(((helper = (helper = helpers.contact || (depth0 != null ? depth0.contact : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"contact","hash":{},"data":data}) : helper)))
+    + " <a href=\"mailto:"
+    + alias4(((helper = (helper = helpers.email || (depth0 != null ? depth0.email : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"email","hash":{},"data":data}) : helper)))
+    + "\" target=\"_blank\" class=\"link link--about\">"
+    + alias4(((helper = (helper = helpers.email || (depth0 != null ? depth0.email : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"email","hash":{},"data":data}) : helper)))
+    + "</a>\n			</p>\n		</div>\n	</div>\n</div>\n\n<!-- socials footer -->\n<footer class=\"sfi sfi--about animate animate--fade-in-up animate--delay-medium\">\n	<a href=\"https://www.behance.net/tk305\"  target=\"_blank\" class=\"sfi__icon\" include=\"../assets/img/behance.svg\"></a>	\n	<a href=\"https://www.instagram.com/tdenheld/\" target=\"_blank\" class=\"sfi__icon\" include=\"../assets/img/insta.svg\"></a>	\n	<a href=\"https://twitter.com/tdenheld\" target=\"_blank\" class=\"sfi__icon\" include=\"../assets/img/twitter.svg\"></a>	\n	<a href=\"https://soundcloud.com/tiborkocsis\" target=\"_blank\" class=\"sfi__icon\" include=\"../assets/img/soundcloud.svg\"></a>	\n</footer>";
+},"useData":true});
+templates['app'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "			<li class=\"navbar__list-item navbar__list-item--"
+    + alias4(((helper = (helper = helpers.item || (depth0 != null ? depth0.item : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"item","hash":{},"data":data}) : helper)))
+    + " js-nav js-nav--"
+    + alias4(((helper = (helper = helpers.item || (depth0 != null ? depth0.item : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"item","hash":{},"data":data}) : helper)))
+    + " opacity-hover\">\n				"
+    + alias4(((helper = (helper = helpers.item || (depth0 != null ? depth0.item : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"item","hash":{},"data":data}) : helper)))
+    + "\n			</li>\n";
+},"3":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "	<div id=\""
+    + alias4(((helper = (helper = helpers.page || (depth0 != null ? depth0.page : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"page","hash":{},"data":data}) : helper)))
+    + "\" class=\"js-page js-page--"
+    + alias4(((helper = (helper = helpers.page || (depth0 != null ? depth0.page : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"page","hash":{},"data":data}) : helper)))
+    + "\"></div>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, alias1=depth0 != null ? depth0 : (container.nullContext || {});
+
+  return "<!-- navbar -->\n<header class=\"navbar animate animate--fade-in-down animate--delay-short\" id=\"js-navbar\">\n	<a href=\"/\">\n		<span include=\"../assets/img/logo-tk305.svg\" class=\"opacity-hover\"></span>\n	</a>\n	<ul class=\"navbar__list\">\n"
+    + ((stack1 = helpers.each.call(alias1,((stack1 = (depth0 != null ? depth0.app : depth0)) != null ? stack1.navbar : stack1),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "	</ul>\n</header>\n\n<div class=\"lines\">\n	<div class=\"lines__line js-line\"><span></span></div>\n	<div class=\"lines__line js-line\"><span></span></div>\n	<div class=\"lines__line js-line\"><span></span></div>\n	<div class=\"lines__line js-line\"><span></span></div>\n	<div class=\"lines__line js-line\"><span></span></div>\n</div>\n\n<!-- pages -->\n"
+    + ((stack1 = helpers.each.call(alias1,((stack1 = (depth0 != null ? depth0.app : depth0)) != null ? stack1.pages : stack1),{"name":"each","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
+templates['case'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "					<h1 class=\"intro__title txt-xxxl animate animate--fade-in\">"
+    + container.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"title","hash":{},"data":data}) : helper)))
+    + "</h1>\n";
+},"3":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return "				<div class=\"case__hero-gradient tr tr--fade-in js-hero-gradient\"></div>\n				<div class=\"case__specs\">\n					<div class=\"case__specs-grid\">\n"
+    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.specs : depth0),{"name":"each","hash":{},"fn":container.program(4, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "					</div>\n				</div>\n";
+},"4":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function";
+
+  return "							<div class=\"case__specs-item tr-stag tr-stag--delay js-specs\">\n								<h3 class=\"case__specs-title tr-stag tr-stag--delay js-specs\">"
+    + ((stack1 = ((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper))) != null ? stack1 : "")
+    + "</h3>\n								<p class=\"case__specs-description tr-stag tr-stag--delay js-specs\">"
+    + ((stack1 = ((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper))) != null ? stack1 : "")
+    + "</p>\n							</div>\n";
+},"6":function(container,depth0,helpers,partials,data,blockParams,depths) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "				<div class=\"case__item case__item--"
+    + alias4(((helper = (helper = helpers.modifier || (depth0 != null ? depth0.modifier : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"modifier","hash":{},"data":data}) : helper)))
+    + " js-case\" id=\"js-case-item-"
+    + alias4(container.lambda((depths[1] != null ? depths[1].page : depths[1]), depth0))
+    + "-"
+    + alias4(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"index","hash":{},"data":data}) : helper)))
+    + "\">\n"
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.text : depth0),{"name":"if","hash":{},"fn":container.program(7, data, 0, blockParams, depths),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.img : depth0),{"name":"if","hash":{},"fn":container.program(9, data, 0, blockParams, depths),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "				</div>\n";
+},"7":function(container,depth0,helpers,partials,data) {
+    var stack1, alias1=container.lambda, alias2=container.escapeExpression;
+
+  return "						<div class=\"case__text-container\">\n							<div class=\"case__line tr-stag js-case-item\"></div>\n							<h2 class=\"txt-xl tr-stag js-case-item\">"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.text : depth0)) != null ? stack1.title : stack1), depth0))
+    + "</h2>\n							<p class=\"case__description tr-stag js-case-item\">"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.text : depth0)) != null ? stack1.description : stack1), depth0))
+    + "</p>\n						</div>\n";
+},"9":function(container,depth0,helpers,partials,data,blockParams,depths) {
+    var helper, alias1=container.escapeExpression;
+
+  return "					<div class=\"img-container tr tr--scale-in js-case-item\">\n						<img data-src=\"media/"
+    + alias1(((helper = (helper = helpers.img || (depth0 != null ? depth0.img : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"img","hash":{},"data":data}) : helper)))
+    + "\" src=\"\" class=\"img-responsive js-loader-item-"
+    + alias1(container.lambda((depths[1] != null ? depths[1].page : depths[1]), depth0))
+    + "\" />\n					</div>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data,blockParams,depths) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<div class=\"case case--"
+    + alias4(((helper = (helper = helpers.page || (depth0 != null ? depth0.page : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"page","hash":{},"data":data}) : helper)))
+    + "\">\n	<div class=\"case__intro-container js-parallax\">\n		<div class=\"intro intro--detail\">\n			<div class=\"intro__container\">\n				<img src=\"media/"
+    + alias4(((helper = (helper = helpers.page || (depth0 != null ? depth0.page : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"page","hash":{},"data":data}) : helper)))
+    + "/logo.svg\" class=\"case__logo animate animate--fade-in\" />\n"
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.title : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0, blockParams, depths),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "				<p class=\"intro__description txt-l animate animate--fade-in animate--delay-short\">"
+    + alias4(((helper = (helper = helpers.intro || (depth0 != null ? depth0.intro : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"intro","hash":{},"data":data}) : helper)))
+    + "</p>\n			</div>\n			<div class=\"scroll-down scroll-down--bottom animate animate--fade-in-up animate--delay-medium\">\n				<div class=\"scroll-down__icon\">\n					<div class=\"scroll-down__icon-circle\"></div>\n				</div>\n				<p class=\"scroll-down__label\">scroll down to discover</p>\n			</div>\n		</div>\n	</div>\n	\n	<div class=\"case__content animate animate--fade-in\">\n		<div class=\"case__hero-container\" id=\"js-hero-"
+    + alias4(((helper = (helper = helpers.page || (depth0 != null ? depth0.page : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"page","hash":{},"data":data}) : helper)))
+    + "\">\n			<img data-src=\"media/"
+    + alias4(((helper = (helper = helpers.hero || (depth0 != null ? depth0.hero : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"hero","hash":{},"data":data}) : helper)))
+    + "\" src=\"\" class=\"case__hero-img js-loader-hero-"
+    + alias4(((helper = (helper = helpers.page || (depth0 != null ? depth0.page : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"page","hash":{},"data":data}) : helper)))
+    + "\" />\n"
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.specs : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0, blockParams, depths),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "		</div>\n		<div class=\"container\">\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.item : depth0),{"name":"each","hash":{},"fn":container.program(6, data, 0, blockParams, depths),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "		</div>\n	</div>\n</div>";
+},"useData":true,"useDepths":true});
+templates['home'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<!-- animation of little objects -->\n<div class=\"ao hidden-xs\">\n	<div class=\"ao__particle ao--move-up-down ao--v-70\"></div>\n	<div class=\"ao__particle ao--move-up-down ao--v-20 ao--delay-2s\"></div>\n	<div class=\"ao__particle ao--move-up-down ao--v-22 ao--delay-3s ao--dur-11s\"></div>\n	<div class=\"ao__particle ao--move-up-down ao--v-30 ao--delay-5s\"></div>\n	<div class=\"ao__particle ao--move-up-down ao--v-50 ao--delay-7s ao--dur-10s\"></div>\n	<div class=\"ao__particle ao--move-up-down ao--v-80 ao--delay-6s ao--dur-13s\"></div>	\n</div>\n\n<!-- intro -->\n<div class=\"intro animate animate--fade-in\">\n	<div class=\"intro__container\">\n		<h1 class=\"intro__title txt-xxxxl\">\n			Creating digital experiences to engage people.\n		</h1>\n		<button class=\"button txt-m js-checkout-button\">check out latest work</button>\n	</div>\n</div>\n\n<!-- socials footer -->\n<footer class=\"sfi animate animate--fade-in-up animate--delay-medium\">\n	<a href=\"https://www.behance.net/tk305\"  target=\"_blank\" class=\"sfi__icon\" include=\"../assets/img/behance.svg\"></a>	\n	<a href=\"https://www.instagram.com/tdenheld/\" target=\"_blank\" class=\"sfi__icon\" include=\"../assets/img/insta.svg\"></a>	\n	<a href=\"https://twitter.com/tdenheld\" target=\"_blank\" class=\"sfi__icon\" include=\"../assets/img/twitter.svg\"></a>	\n	<a href=\"https://soundcloud.com/tiborkocsis\" target=\"_blank\" class=\"sfi__icon\" include=\"../assets/img/soundcloud.svg\"></a>	\n</footer>";
+},"useData":true});
+templates['work'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "			<div class=\"tile tile--"
+    + alias4(((helper = (helper = helpers.page || (depth0 != null ? depth0.page : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"page","hash":{},"data":data}) : helper)))
+    + " js-case--"
+    + alias4(((helper = (helper = helpers.page || (depth0 != null ? depth0.page : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"page","hash":{},"data":data}) : helper)))
+    + " animate animate--fade-in\">\n				<div class=\"tile__link\"></div>\n				<h2 class=\"tile__title\">"
+    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</h2>\n				<p class=\"tile__description\">"
+    + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
+    + "</p>\n			</div>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {});
+
+  return "<div class=\"work\">\n	<h1 class=\"work__title\">"
+    + container.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</h1>\n	<div class=\"work__grid\">\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.work : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "	</div>\n</div>";
+},"useData":true});
+})();
---
+++ assets/js/tk.generic.js
@@ -0,0 +1,371 @@
+// TK305 - JAVASCRIPT
+
+$(document).ready(function(){
+	
+	// global vars
+	// ------------------------------------------------------------
+	
+	// screen handler vars
+	var mobile = false;
+	var window_width = Number;
+	var window_height = Number;
+	
+	// settings on scroll
+	var hero_bottom = Number;
+	var trigger_hook = Number;
+	
+	// animations
+	var default_ease = Power3.easeInOut;
+	
+	
+
+	
+	
+	// screen sizes handler
+	// ------------------------------------------------------------	
+		
+	// update screen
+	function update_window_size() {
+   		window_height = $(window).innerHeight();
+   		window_width = $(window).innerWidth();
+   		hero_bottom = Math.round(window_width * .625);
+   		
+   		// set breakpoints
+   		if (window_width > 912) {
+   			mobile = false;
+   			trigger_hook = 0.8;
+   		} else {
+	   		mobile = true;
+	   		trigger_hook = 0.9;
+   		};
+  	};  	
+  	update_window_size();
+  	
+	// update when resizing
+	$(window).resize(function() {
+		update_window_size();
+	});
+	
+	
+	
+	
+	
+	
+	
+	// scroll magic
+	// -----------------------------------
+	// -----------------------------------
+	
+	// init controller
+	var controller = new ScrollMagic.Controller({
+		globalSceneOptions: {
+			duration: 99999,
+		},
+	});
+	
+	// constructor for case items
+	function caseItemScrollEvent(id) {
+		var element_id = document.getElementById(id);
+		var selector = "#" + id + " .js-case-item";
+		
+		if (element_id) {
+			var scrll = new ScrollMagic
+			.Scene({triggerElement: "#" + id})
+			.triggerHook(trigger_hook)
+			.setClassToggle(selector, "is-active")
+			//.addIndicators()
+			.addTo(controller);
+		};
+	};
+	
+	// loop trough all case items on all pages
+	function scrollEvents(page) {
+		setTimeout(function(){
+			for (n = 0; n < 20; n++) {
+				var id = "js-case-item-" + page + "-" + n;
+				caseItemScrollEvent(id);
+			};
+		}, 600);
+	};	
+	
+	// constructor for navbar color change
+	// -----------------------------------
+	function navColorChange(id) {
+		var selector = "#js-hero-" + id;
+		var scrll = new ScrollMagic.Scene({
+			triggerElement:	selector,
+		})
+		.offset(hero_bottom - 28)
+		.triggerHook(0)
+		.setClassToggle("#js-navbar", "navbar-color--" + id)
+		//.addIndicators()
+		.addTo(controller);
+	};
+	navColorChange("wn");
+	navColorChange("ar");
+	
+	
+	
+	
+	
+	// image loader
+	// ------------------------------------------------------------
+	function img_loader(page) {	
+		var hero = ".js-loader-hero-" + page;
+		var item = ".js-loader-item-" + page;
+		
+		$(hero).Lazy();
+		TweenLite.delayedCall(.05, function(){
+			$(item).Lazy({
+				threshold: 99999,
+				afterLoad: function() {
+					scrollEvents(page);
+				},
+			});
+		}); 
+    };
+	
+		
+	
+
+	
+	// functionality that"s on linked on scroll
+	// ------------------------------------------------------------	
+	var scrolled = false;
+	
+	$(window).scroll(function () {
+		scrolled = true;
+		if (scrolled && mobile == false) {
+			requestAnimationFrame(scroll);
+		};
+	});
+	
+	function scroll() {
+		var pos = $(window).scrollTop();
+		
+		// parallax
+	    if (pos < window_height) {
+			$(".js-parallax").css({"transform" : "translate3d(0," + Math.round(0 - pos / 5.5) + "px ,0)" });
+		};
+		
+		// show hide specs on scroll
+		if (pos > window_height - (window_height - hero_bottom)) {
+			$(".js-hero-gradient").addClass("is-active");
+			$(".js-specs").addClass("is-active");
+		} else {
+			$(".js-hero-gradient").removeClass("is-active");
+			$(".js-specs").removeClass("is-active");
+		};
+		
+		scrolled = false;
+	};
+	
+
+	
+
+	
+	
+	// router
+	// ----------------------------------
+	function router() {
+		var routes = [
+			"home",
+			"work",
+			"about",
+			"ns",
+			"wn",
+			"ar",
+			"cg",
+		];
+		
+		// default state
+		$(".js-page").hide();	
+		$(".js-page--home").show();
+		
+		function toggle_cases(i) {	
+			var btn = ".js-case--" + i;
+			var page = ".js-page--" + i;
+								
+			$(btn).click(function() {
+				TweenLite.set(".js-page", {
+					autoAlpha: 0,
+					display: "none",
+				});
+				TweenLite.set(page, {
+					autoAlpha: 1,
+					display: "block"
+				});
+							
+				$(".js-nav--work").removeClass("is-active");
+				$(".js-nav--about").removeClass("is-active");
+				$(window).scrollTop(0);
+													
+				for (n = 0; n < routes.length; n++) {
+					var r = routes[n];
+					if (page !== ".js-page--" + r) {
+						$("#js-navbar").removeClass("navbar--" + r);
+					} else if (page == ".js-page--" + r) {
+						$("#js-navbar").addClass("navbar--" + r);
+						$("body").removeClass().addClass("bg--" + r);
+						img_loader(r);
+					};
+				};
+			});
+		};
+		
+		// loop trough all pages
+		for (i = 3; i < routes.length; i++) {
+			toggle_cases(routes[i]);
+		};
+		
+		// base function to trigger page transition
+		function page_transition(s, page){
+			TweenLite.to(".js-page", s, {
+				autoAlpha: 0,
+				display: "none",
+				onComplete: function(){
+					TweenLite.set(".js-page--" + page, {
+						autoAlpha: 1,
+						display: "block",
+					});
+					$("body").removeClass().addClass("bg--" + page);
+				},
+			});
+		};
+		
+		// main call to action button on home
+		$(".js-checkout-button").click(function() {
+			$(".js-line").addClass("is-active");
+			$("#js-navbar").removeClass().addClass("navbar");
+			TweenLite.to("#js-navbar", .55, {
+				ease: default_ease,
+				autoAlpha: 0,
+			});
+			img_loader("ns");
+			setTimeout(function() {
+				TweenLite.to(".js-line", .6, {
+					ease: default_ease,
+					autoAlpha: 0,
+					display: "none",
+				});
+				$(window).scrollTop(0);
+			}, 600);
+			setTimeout(function() {
+				$("#js-navbar").addClass("navbar--ns");
+				TweenLite.to("#js-navbar", .7, {
+					ease: default_ease,
+					autoAlpha: 1,
+				});
+			}, 900);
+			page_transition(.6, "ns");
+		});
+		
+		// navbar work
+		$(".js-nav--work").click(function() {
+			$(".js-nav--about").removeClass("is-active");
+			TweenLite.to("#js-navbar", .25, {
+				ease: default_ease,
+				autoAlpha: 0,
+			});
+			setTimeout(function() {
+				// work
+				$(".js-nav--work").addClass("is-active");
+				
+				// navbar
+				$("#js-navbar").removeClass().addClass("navbar navbar--work");
+				TweenLite.to("#js-navbar", .7, {
+					ease: default_ease,
+					autoAlpha: 1,
+				});
+				
+				// remove lines in the background
+				$(".js-line").removeClass("is-active");
+				TweenLite.to(".js-line", .6, {
+					ease: default_ease,
+					autoAlpha: 0,
+					display:"none",
+				});
+				
+				// scroll to top
+				$(window).scrollTop(0);
+			}, 300);
+			page_transition(.25, "work");
+		});
+		
+		// navbar about
+		$(".js-nav--about").click(function() {
+			$(".js-nav--work").removeClass("is-active");
+			TweenLite.to("#js-navbar", .25, {
+				ease: default_ease,
+				autoAlpha: 0,
+			});		    
+		    // about image
+		    $(".js-about-img").Lazy({
+				threshold: 99999,
+			});
+			setTimeout(function() {
+				// about
+				$(".js-nav--about").addClass("is-active");
+				
+				// navbar
+				$("#js-navbar").removeClass().addClass("navbar navbar--about");
+				TweenLite.to("#js-navbar", .7, {
+					ease: default_ease,
+					autoAlpha: 1,
+				});
+				
+				// add lines in the background
+				$(".js-line").removeClass("is-active");
+				TweenLite.set(".js-line", {
+					autoAlpha: 1,
+					display: "block",
+				});
+				
+				// scroll to top
+				$(window).scrollTop(0);
+			}, 300);
+			page_transition(.25, "about");
+		});
+	};
+	router();
+	
+  
+    
+	
+	
+	// include html
+	// ------------------------------------------------------------
+	function include_html() {
+	    var z, i, elmnt, file, xhttp;
+	    /*loop through a collection of all HTML elements:*/
+	    z = document.getElementsByTagName("*");
+	    for (i = 0; i < z.length; i++) {
+	        elmnt = z[i];
+	        /*search for elements with a certain atrribute:*/
+	        file = elmnt.getAttribute("include");
+	        if (file) {
+	        	/*make an HTTP request using the attribute value as the file name:*/
+	            xhttp = new XMLHttpRequest();
+	            xhttp.onreadystatechange = function () {
+	                if (this.readyState == 4) {
+	                    if (this.status == 200) {
+	                        elmnt.innerHTML = this.responseText;
+	                    };
+	                    if (this.status == 404) {
+	                        elmnt.innerHTML = "Page not found.";
+	                    };
+	                    /*remove the attribute, and call this function once more:*/
+	                    elmnt.removeAttribute("include");
+	                    include_html();
+	                };
+	            };
+	            xhttp.open("GET", file, true);
+	            xhttp.send();
+	            /*exit the function:*/
+	            return;
+	        };
+	    };
+	};
+	include_html();	
+
+});
