67652e598b85982336b7ab1b3c1147751675e187
Tibor <tibor@tk305.com>
2018-09-12 13:41:35 +0200

    cleanup

--- assets/js/tk.generic.js
+++ assets/js/tk.generic.js
@@ -1,96 +1,120 @@
 // TK305 - JAVASCRIPT
 
-$(document).ready(function(){
-	
+$(document).ready(function () {
+
 	// global vars
 	// ------------------------------------------------------------
-	
+
 	// screen handler vars
 	var mobile = false;
 	var window_width = Number;
 	var window_height = Number;
-	
+
 	// settings on scroll
 	var hero_bottom = Number;
 	var trigger_hook = Number;
-	
+
 	// animations
 	var default_ease = Power3.easeInOut;
-	
-	
 
-	
-	
+
+
+
+
 	// screen sizes handler
 	// ------------------------------------------------------------	
-		
+
 	// update screen
 	function update_window_size() {
-   		window_height = $(window).innerHeight();
-   		window_width = $(window).innerWidth();
-   		hero_bottom = Math.round(window_width * .625);
-   		
-   		// set breakpoints
-   		if (window_width > 912) {
-   			mobile = false;
-   			trigger_hook = 0.8;
-   		} else {
-	   		mobile = true;
-	   		trigger_hook = 0.9;
-   		};
-  	};  	
-  	update_window_size();
-  	
+		window_height = $(window).innerHeight();
+		window_width = $(window).innerWidth();
+		hero_bottom = Math.round(window_width * .625);
+
+		// set breakpoints
+		if (window_width > 912) {
+			mobile = false;
+			trigger_hook = 0.8;
+		} else {
+			mobile = true;
+			trigger_hook = 0.9;
+		};
+	};
+	update_window_size();
+
 	// update when resizing
-	$(window).resize(function() {
+	$(window).resize(function () {
 		update_window_size();
 	});
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 	// scroll magic
 	// ------------------------------------------------------------------
 	// ------------------------------------------------------------------
-	
+
 	// init controller
 	var controller = new ScrollMagic.Controller({
 		globalSceneOptions: {
 			duration: 99999,
 		},
 	});
-	
+
 	// constructor for case items
 	function caseItemScrollEvent(id) {
 		var element_id = document.getElementById(id);
 		var selector = "#" + id + " .js-case-item";
-		
+
 		if (element_id) {
 			var scrll = new ScrollMagic.Scene({
-				triggerElement: "#" + id,
-			})
-			.triggerHook(trigger_hook)
-			.setClassToggle(selector, "is-active")
-			//.addIndicators()
-			.addTo(controller);
+					triggerElement: "#" + id,
+				})
+				.triggerHook(trigger_hook)
+				.setClassToggle(selector, "is-active")
+				//.addIndicators()
+				.addTo(controller);
 		};
 	};
-	
 	// loop trough all case items on all pages
 	function scrollEvents(page) {
 		var case_item = $(".js-case");
-		setTimeout(function(){
+		setTimeout(function () {
 			for (n = 0; n < case_item.length; n++) {
 				var id = "js-case-item-" + page + "-" + n;
 				caseItemScrollEvent(id);
 			};
 		}, 600);
-	};	
-	
-	
+	};
+
+	// generic scrollmagic constructor
+	// -----------------------------------------------------------------
+	function scroll_trig(i) {
+		var id = document.getElementById("scrll-section-" + i);
+		var el = "#scrll-section-" + i;
+		if (id) {
+			var scrll = new ScrollMagic.Scene({
+					triggerElement: el,
+				})
+				.triggerHook(trigger_hook)
+				.offset(0)
+				.on("start", function () {
+					$(el + " .tr").toggleClass("is-active");
+					$(el + " .tr-stag").toggleClass("is-active");
+				})
+				//.addIndicators()
+				.addTo(controller);
+		};
+	};
+	// init section scroll events
+	var sections = $(".js-scrll-section");
+	sections.each(function (i) {
+		$(this).attr("id", "scrll-section-" + i);
+		scroll_trig(i);
+	});
+
 	// constructor for navbar color change
 	// ----------------------------------------------------------------
 	function navColorChange(id) {
@@ -106,78 +130,52 @@ $(document).ready(function(){
 	};
 	navColorChange("wn");
 	navColorChange("ar");
-	
-	
-	// generic scrollmagic constructor
-	// -----------------------------------------------------------------
-	function scroll_trig(i) {
-		var id = document.getElementById("scrll-section-" + i);
-		var el = "#scrll-section-" + i;
-		if (id) {
-			var scrll = new ScrollMagic.Scene({
-				triggerElement:	el,
-			})
-			.triggerHook(trigger_hook)
-			.offset(0)
-			.on("start", function(){
-				$(el + " .tr").toggleClass("is-active");
-				$(el + " .tr-stag").toggleClass("is-active");
-			})
-			//.addIndicators()
-			.addTo(controller);
-		};
-	};
-	// init section scroll events
-	var sections = $(".js-scrll-section");
-	sections.each(function(i){
-		$(this).attr("id", "scrll-section-" + i);
-		scroll_trig(i);
-	});
-	
-	
-	
-	
-	
+
+
+
+
+
 	// image loader
 	// ------------------------------------------------------------
-	function img_loader(page) {	
+	function img_loader(page) {
 		var hero = ".js-loader-hero-" + page;
 		var item = ".js-loader-item-" + page;
-		
+
 		$(hero).Lazy();
-		TweenLite.delayedCall(.05, function(){
+		TweenLite.delayedCall(.05, function () {
 			$(item).Lazy({
 				threshold: 99999,
-				afterLoad: function() {
+				afterLoad: function () {
 					scrollEvents(page);
 				},
 			});
-		}); 
-    };
-	
-		
-	
+		});
+	};
 
 	
+
+
 	// functionality that"s on linked on scroll
 	// ------------------------------------------------------------	
 	var scrolled = false;
-	
+
 	$(window).scroll(function () {
 		scrolled = true;
 		if (scrolled && mobile == false) {
 			requestAnimationFrame(scroll);
 		};
 	});
-	
+
 	function scroll() {
 		var pos = $(window).scrollTop();
-		
+
 		// parallax
-	    if (pos < window_height) {
-			$(".js-parallax").css({"transform" : "translate3d(0," + Math.round(0 - pos / 5.5) + "px ,0)" });
+		if (pos < window_height) {
+			$(".js-parallax").css({
+				"transform": "translate3d(0," + Math.round(0 - pos / 5.5) + "px ,0)"
+			});
 		};
-		
+
 		// show hide specs on scroll
 		if (pos > window_height - (window_height - hero_bottom)) {
 			$(".js-hero-gradient").addClass("is-active");
@@ -186,21 +184,21 @@ $(document).ready(function(){
 			$(".js-hero-gradient").removeClass("is-active");
 			$(".js-specs").removeClass("is-active");
 		};
-		
+
 		scrolled = false;
 	};
-	
 
-	
 
-	
-	
+
+
+
+
 	// router
 	// ----------------------------------
 	function router() {
 		var home = true;
 		var about = false;
-						
+
 		var routes = [
 			"home",
 			"work",
@@ -210,16 +208,16 @@ $(document).ready(function(){
 			"ar",
 			"cg",
 		];
-		
+
 		// default state
-		$(".js-page").hide();	
+		$(".js-page").hide();
 		$(".js-page--home").show();
-		
-		function toggle_cases(i) {	
+
+		function toggle_cases(i) {
 			var btn = ".js-case--" + i;
 			var page = ".js-page--" + i;
-								
-			$(btn).click(function() {
+
+			$(btn).click(function () {
 				TweenLite.set(".js-page", {
 					autoAlpha: 0,
 					display: "none",
@@ -228,11 +226,11 @@ $(document).ready(function(){
 					autoAlpha: 1,
 					display: "block"
 				});
-							
+
 				$(".js-nav--work").removeClass("is-active");
 				$(".js-nav--about").removeClass("is-active");
 				$(window).scrollTop(0);
-													
+
 				for (n = 0; n < routes.length; n++) {
 					var r = routes[n];
 					if (page !== ".js-page--" + r) {
@@ -245,18 +243,18 @@ $(document).ready(function(){
 				};
 			});
 		};
-		
+
 		// loop trough all pages
 		for (i = 3; i < routes.length; i++) {
 			toggle_cases(routes[i]);
 		};
-		
+
 		// base function to trigger page transition
-		function page_transition(s, page){
+		function page_transition(s, page) {
 			TweenLite.to(".js-page", s, {
 				autoAlpha: 0,
 				display: "none",
-				onComplete: function(){
+				onComplete: function () {
 					TweenLite.set(".js-page--" + page, {
 						autoAlpha: 1,
 						display: "block",
@@ -265,9 +263,9 @@ $(document).ready(function(){
 				},
 			});
 		};
-		
+
 		// main call to action button on home
-		$(".js-checkout-button").click(function() {
+		$(".js-checkout-button").click(function () {
 			home = false;
 			$(".js-line").addClass("is-active");
 			$("#js-navbar").removeClass().addClass("navbar");
@@ -276,7 +274,7 @@ $(document).ready(function(){
 				autoAlpha: 0,
 			});
 			img_loader("ns");
-			setTimeout(function() {
+			setTimeout(function () {
 				TweenLite.to(".js-line", .6, {
 					ease: default_ease,
 					autoAlpha: 0,
@@ -284,7 +282,7 @@ $(document).ready(function(){
 				});
 				$(window).scrollTop(0);
 			}, 600);
-			setTimeout(function() {
+			setTimeout(function () {
 				$("#js-navbar").addClass("navbar--ns");
 				TweenLite.to("#js-navbar", .7, {
 					ease: default_ease,
@@ -293,9 +291,9 @@ $(document).ready(function(){
 			}, 900);
 			page_transition(.6, "ns");
 		});
-		
+
 		// navbar work
-		$(".js-nav--work").click(function() {
+		$(".js-nav--work").click(function () {
 			home = false;
 			about = false;
 			$(".js-nav--about").removeClass("is-active");
@@ -303,33 +301,33 @@ $(document).ready(function(){
 				ease: default_ease,
 				autoAlpha: 0,
 			});
-			setTimeout(function() {
+			setTimeout(function () {
 				// work
 				$(".js-nav--work").addClass("is-active");
-				
+
 				// navbar
 				$("#js-navbar").removeClass().addClass("navbar navbar--work");
 				TweenLite.to("#js-navbar", .7, {
 					ease: default_ease,
 					autoAlpha: 1,
 				});
-				
+
 				// remove lines in the background
 				$(".js-line").removeClass("is-active");
 				TweenLite.to(".js-line", .6, {
 					ease: default_ease,
 					autoAlpha: 0,
-					display:"none",
+					display: "none",
 				});
-				
+
 				// scroll to top
 				$(window).scrollTop(0);
 			}, 300);
 			page_transition(.25, "work");
 		});
-		
+
 		// navbar about
-		$(".js-nav--about").click(function() {
+		$(".js-nav--about").click(function () {
 			// if on same page scroll up
 			if (about) {
 				TweenLite.to(window, 0.6, {
@@ -339,7 +337,7 @@ $(document).ready(function(){
 						autoKill: false,
 					},
 				});
-			// execute page transition when not on about page
+				// execute page transition when not on about page
 			} else {
 				about = true;
 				$(".js-nav--work").removeClass("is-active");
@@ -347,16 +345,16 @@ $(document).ready(function(){
 					TweenLite.to("#js-navbar", .25, {
 						ease: default_ease,
 						autoAlpha: 0,
-					});		
-				};  
-			    // about image
-			    $(".js-about-img").Lazy({
+					});
+				};
+				// about image
+				$(".js-about-img").Lazy({
 					threshold: 99999,
 				});
-				setTimeout(function() {
+				setTimeout(function () {
 					// about
 					$(".js-nav--about").addClass("is-active");
-					
+
 					// navbar
 					$("#js-navbar").removeClass().addClass("navbar navbar--about");
 					if (home == false) {
@@ -365,14 +363,14 @@ $(document).ready(function(){
 							autoAlpha: 1,
 						});
 					}
-					
+
 					// add lines in the background
 					$(".js-line").removeClass("is-active");
 					TweenLite.set(".js-line", {
 						autoAlpha: 1,
 						display: "block",
 					});
-					
+
 					// scroll to top
 					$(window).scrollTop(0);
 				}, 300);
@@ -381,44 +379,44 @@ $(document).ready(function(){
 		});
 	};
 	router();
-	
-  
-    
-	
-	
+
+
+
+
+
 	// include html
 	// ------------------------------------------------------------
 	function include_html() {
-	    var z, i, elmnt, file, xhttp;
-	    /*loop through a collection of all HTML elements:*/
-	    z = document.getElementsByTagName("*");
-	    for (i = 0; i < z.length; i++) {
-	        elmnt = z[i];
-	        /*search for elements with a certain atrribute:*/
-	        file = elmnt.getAttribute("include");
-	        if (file) {
-	        	/*make an HTTP request using the attribute value as the file name:*/
-	            xhttp = new XMLHttpRequest();
-	            xhttp.onreadystatechange = function () {
-	                if (this.readyState == 4) {
-	                    if (this.status == 200) {
-	                        elmnt.innerHTML = this.responseText;
-	                    };
-	                    if (this.status == 404) {
-	                        elmnt.innerHTML = "Page not found.";
-	                    };
-	                    /*remove the attribute, and call this function once more:*/
-	                    elmnt.removeAttribute("include");
-	                    include_html();
-	                };
-	            };
-	            xhttp.open("GET", file, true);
-	            xhttp.send();
-	            /*exit the function:*/
-	            return;
-	        };
-	    };
+		var z, i, elmnt, file, xhttp;
+		/*loop through a collection of all HTML elements:*/
+		z = document.getElementsByTagName("*");
+		for (i = 0; i < z.length; i++) {
+			elmnt = z[i];
+			/*search for elements with a certain atrribute:*/
+			file = elmnt.getAttribute("include");
+			if (file) {
+				/*make an HTTP request using the attribute value as the file name:*/
+				xhttp = new XMLHttpRequest();
+				xhttp.onreadystatechange = function () {
+					if (this.readyState == 4) {
+						if (this.status == 200) {
+							elmnt.innerHTML = this.responseText;
+						};
+						if (this.status == 404) {
+							elmnt.innerHTML = "Page not found.";
+						};
+						/*remove the attribute, and call this function once more:*/
+						elmnt.removeAttribute("include");
+						include_html();
+					};
+				};
+				xhttp.open("GET", file, true);
+				xhttp.send();
+				/*exit the function:*/
+				return;
+			};
+		};
 	};
-	include_html();	
+	include_html();
 
 });
